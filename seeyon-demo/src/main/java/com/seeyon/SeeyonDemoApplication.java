5dbfeee62998d6d0e1c92554f5db959a9efe26b1
chf <948271656@qq.com>
2020-10-17 23:29:30 +0800

    初始化项目

---
+++ seeyon-demo/src/main/java/com/seeyon/SeeyonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.seeyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeyonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeyonDemoApplication.class, args);
+    }
+
+}
